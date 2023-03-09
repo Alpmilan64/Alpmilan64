2b729aa58befd62dea553fec85867e5202347b91
Alpmilan64 <117842731+Alpmilan64@users.noreply.github.com>
2023-03-09 15:06:12 -0500

    add new project

--- src/test/java/com/neotech/testbase/BaseClass.java
+++ src/test/java/com/neotech/testbase/BaseClass.java
@@ -1,7 +1,6 @@
 package com.neotech.testbase;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
